61285e3772768c70268fb268c671b2957d77bec2
Ravikiran Mothukuri <75983016+ravikiran-mothukuri@users.noreply.github.com>
2025-02-15 00:16:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-// import './css/styles.css';
-// import './css/Bstyles.css';
-// import './css/Airbnb.css';
-// import './css/Travel.css';
-// import './css/Meme.css';
-// import './css/Card.css';
-// import './css/Box.css';
-// import './css/Form.css';
 import './css/styles.css';
 
 
