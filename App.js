14825a8b4d0497b6cf8c9ec531739bf91d0b9d23
Ravikiran Mothukuri <75983016+ravikiran-mothukuri@users.noreply.github.com>
2025-02-15 00:15:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+//import Helper from './components/Airbub/Helper';
+//import TravelPage from './components/Airbub/TravelPage';
+//import arr from './components/Airbub/ex';
+// import Main from './components/Meme/Main';
+//import List from './components/List';
+// import LikeCard from './components/FavoriteCard/Body';
+//import CounterBody from './components/Counter/CounterBody';
+// import Box from './components/Boxes/boxMain';
+// import JokeMain from './components/Joke/JokesMain';
+// import Form from './components/KeyStroke/Form';
+// import FormB from './components/FormBasic/Form';
+// import Main from './components/Fetches/BasicFetch';
+// import Counter from './components/Fetches/Counter';
+// import Window from './components/WindowSize/Window';
+import WhyReact from './components/React_Diff_Modes/WhyReact';
+
+function App() {
+  // const firstName= "RaviKiran";
+  // const lastName= "Mothukuri";
+
+  return (
+    <div>
+      {/* <Helper />  */}
+      {/* <TravelPage /> */}
+      {/* <Main /> */}
+      {/* <CounterBody /> */}
+      <WhyReact />
+      {/* <List /> */}
+      {/* <LikeCard /> */}
+      {/* <Box /> */}
+      {/* <hr></hr>
+      <hr></hr> */}
+      {/* <JokeMain /> */}
+      {/* <Form /> */}
+      {/* <FormB /> */}
+      {/* <Counter /> */}
+      {/* <Window /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom/client'; 
+
+import App from './App';
+
+// import './css/styles.css';
+// import './css/Bstyles.css';
+// import './css/Airbnb.css';
+// import './css/Travel.css';
+// import './css/Meme.css';
+// import './css/Card.css';
+// import './css/Box.css';
+// import './css/Form.css';
+import './css/styles.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App/>
+);
+
